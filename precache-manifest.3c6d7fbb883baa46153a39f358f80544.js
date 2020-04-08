b1676678eb0d80791109ab09ad889465caa790d8
amurto <amurtobasu@gmail.com>
2020-04-08 19:30:58 +0530

    deploy ready

---
+++ precache-manifest.3c6d7fbb883baa46153a39f358f80544.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0ba7e5cf45c821ee69d812f81827e5",
+    "url": "/jeev-rakshak/index.html"
+  },
+  {
+    "revision": "3d175015df2f65e40f66",
+    "url": "/jeev-rakshak/static/css/main.308f3d26.chunk.css"
+  },
+  {
+    "revision": "09343d5de57f12386c2d",
+    "url": "/jeev-rakshak/static/js/2.1d3882fa.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/jeev-rakshak/static/js/2.1d3882fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d175015df2f65e40f66",
+    "url": "/jeev-rakshak/static/js/main.7d6b4bbf.chunk.js"
+  },
+  {
+    "revision": "afdbb80fb1080b2c6062",
+    "url": "/jeev-rakshak/static/js/runtime-main.5e2d25b1.js"
+  },
+  {
+    "revision": "aba3531c54d2a85939da13dda7b93350",
+    "url": "/jeev-rakshak/static/media/keras.aba3531c.png"
+  },
+  {
+    "revision": "e1099df75788b29bcffd8bbdafd3647f",
+    "url": "/jeev-rakshak/static/media/react.e1099df7.png"
+  },
+  {
+    "revision": "df661c755b80475d89b5191f05b72dd1",
+    "url": "/jeev-rakshak/static/media/tf.df661c75.png"
+  }
+]);
